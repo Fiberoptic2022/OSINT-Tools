b574b9dfa7ac30951cd00871d9477464e0d30153
Daniel Fiberoptic <fiberopticpy2022@gmail.com>
2024-08-10 10:36:51 -0400

    read me and setup script

--- setup.bat
+++ setup.bat
@@ -5,3 +5,9 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/Fiberoptic2022/OSINT-Tools
 git push -u origin main
+
+
+git config --global user.email "fiberopticpy2022@gmail.com"
+git config --global user.name "Daniel Fiberoptic"
+
+
